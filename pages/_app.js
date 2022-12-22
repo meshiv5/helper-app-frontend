0a19c231a7689eda1245720e046dbe0d091585f6
meshiv5 <000done9@gmail.com>
2022-12-22 15:30:34 +0530

    Edited

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { Provider, useSelector } from "react-redux";
+import { store } from "../redux/store";
+import WithAction from "./Navbar/Navbar";
 
-import { ChakraProvider } from '@chakra-ui/react'
-import { Provider } from 'react-redux'
-import { store } from '../redux/store'
-import WithAction from './Navbar/Navbar'
-
-export default function App({ Component, pageProps }) {
-  return <ChakraProvider>
-      <Provider store={store}>
-        <WithAction />
-        <Component {...pageProps} />
-      </Provider>
+export function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isAuth = useSelector((s) => s.auth.isAuth);
+  useEffect(() => {
+    if (!isAuth) router.push("/auth/login");
+  }, [isAuth]);
+  return (
+    <ChakraProvider>
+      <WithAction />
+      <Component {...pageProps} />
     </ChakraProvider>
-  
-
+  );
 }
-
+export default function PrivateRoute({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <App Component={Component} pageProps={pageProps} />
+    </Provider>
+  );
+}
