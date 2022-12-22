965fdcd5105aee6a2ddd558fd1999c6cbb17804f
meshiv5 <000done9@gmail.com>
2022-12-22 21:05:01 +0530

    D

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     secret: "785c1f49324d6a415490",
     cluster: "ap2",
     access_token_secret: "FkMKR512PxT%JiV!o8U%",
-    server: "http://localhost:8000/",
+    server: "https://rectangular-petal-snowdrop.glitch.me/",
   },
 };
 
